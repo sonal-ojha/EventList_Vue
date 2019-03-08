a4e1b7b2f972da20bc53a2c1c457d5fa3f1cafd9
sonalsonu <sonal.ojha@moengage.com>
2019-03-08 12:20:47 +0530

    added validation logic

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -2,11 +2,12 @@ import axios from 'axios'
 
 const apiClient = axios.create({
   baseURL: `http://localhost:3000`,
-  withCredentials: false,
+  withCredentials: false, // This is the default
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
-  }
+  },
+  timeout: 10000
 })
 
 export default {
